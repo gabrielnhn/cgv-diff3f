b093675891852d29811994e343db308598d31621
Gabriel Hishida <gabrielnhn@duck.com>
2025-06-11 17:08:05 +0200

    cleanup

--- external/include/run_python.hpp
+++ external/include/run_python.hpp
@@ -7,7 +7,6 @@
 int run_python(int argc, char* argv[], std::string path)
 {
     const char* python_path = path.c_str();
-
     FILE* file = fopen(python_path, "r");
 
     PyStatus status;
@@ -16,27 +15,37 @@ int run_python(int argc, char* argv[], std::string path)
 
     // Recommended: set program name
     status = PyConfig_SetBytesString(&config, &config.program_name, argv[0]);
-    if (PyStatus_Exception(status)) goto exception;
+    if (PyStatus_Exception(status))
+    {
+        PyConfig_Clear(&config);
+        Py_ExitStatusException(status);
+        return 0;
+    }
 
     status = Py_InitializeFromConfig(&config);
-    if (PyStatus_Exception(status)) goto exception;
+    if (PyStatus_Exception(status))
+    {
+        PyConfig_Clear(&config);
+        Py_ExitStatusException(status);
+        return 0;
+    }
 
     PyConfig_Clear(&config);
 
     if (file != nullptr) {
         PyRun_SimpleFile(file, python_path);
         fclose(file);
-    } else {
-        std::cerr << "Failed to open file: " << python_path << std::endl;
+    }
+    else
+    {
+        std::cout << "Failed to open file: " << python_path << std::endl;
+        return 0;
     }
 
     if (Py_FinalizeEx() < 0) {
+        std::cout << "PyFinalizeEx failed" << std::endl;
         exit(120);
     }
-    return 1;
 
-exception:
-    PyConfig_Clear(&config);
-    Py_ExitStatusException(status);
-    return 0;
+    return 1;
 }
